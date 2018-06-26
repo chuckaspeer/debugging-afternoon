d22adada88f32abe759712971fe5f2c7cfc761b7
chuckaspeer <chuckaspeer@gmail.com>
2018-06-26 16:09:02 -0500

    stage-3

--- stage-3/src/redux/store.js
+++ stage-3/src/redux/store.js
@@ -1,4 +1,9 @@
+import { createStore, applyMiddleware} from "redux";
 import reducer from './reducer';
 import reduxPromiseMiddleware from 'redux-promise-middleware';
 
 export default createStore(reducer, applyMiddleware(reduxPromiseMiddleware()));
+
+
+
+
